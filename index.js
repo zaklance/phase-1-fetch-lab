c7ce8b3410cf94c03e351747b374206de6965f61
zaklance <wosewick@gmail.com>
2024-04-30 15:02:12 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
 }
+
+// fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
